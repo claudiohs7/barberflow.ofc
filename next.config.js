e4e9a9d457ed5af05eb248f8647510e20aae3bdd
Claudio Henrique <claudiohs@hotmail.com>
2025-12-19 11:23:07 -0300

    Fix: Adicionando basePath para subpasta

--- next.config.js
+++ next.config.js
@@ -1,37 +1,31 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  /* Adicione esta linha abaixo */
+  basePath: '/barberflow', 
+  
   typescript: {
     ignoreBuildErrors: true,
-  },
-  experimental: {
-    
   },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'api.qrserver.com',
-        port: '',
         pathname: '/**',
       }
     ],
