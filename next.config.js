26ec088e27f5857048eb3efbd7bd4cd58d03decb
Claudio Henrique <claudiohs@hotmail.com>
2025-12-23 11:13:22 -0300

    23/12 13

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+const rawBasePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const normalizedBasePath =
+  rawBasePath && rawBasePath !== "/"
+    ? `/${rawBasePath.replace(/^\/+|\/+$/g, "")}`
+    : "";
+
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Permite servir o app em um subcaminho (ex.: /barberflow) quando configurado no ambiente
+  basePath: normalizedBasePath || undefined,
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'placeholder.co', pathname: '/**' },
